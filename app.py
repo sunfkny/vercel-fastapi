d85331ae99c35650b311a4199d16579904834491
sunfkny <30853461+sunfkny@users.noreply.github.com>
2022-05-23 15:11:32 +0800

    init

---
+++ app.py
@@ -0,0 +1,59 @@
+from typing import Optional
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse, StreamingResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
+import requests
+
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    # 允许跨域的源列表，例如 ["http://www.example.org"] 等等，["*"] 表示允许任何源
+    allow_origins=["*"],
+    # 跨域请求是否支持 cookie，默认是 False，如果为 True，allow_origins 必须为具体的源，不可以是 ["*"]
+    allow_credentials=False,
+    # 允许跨域请求的 HTTP 方法列表，默认是 ["GET"]
+    allow_methods=["*"],
+    # 允许跨域请求的 HTTP 请求头列表，默认是 []，可以使用 ["*"] 表示允许所有的请求头
+    # 当然 Accept、Accept-Language、Content-Language 以及 Content-Type 总之被允许的
+    allow_headers=["*"],
+    # 可以被浏览器访问的响应头, 默认是 []，一般很少指定
+    # expose_headers=["*"]
+    # 设定浏览器缓存 CORS 响应的最长时间，单位是秒。默认为 600，一般也很少指定
+    # max_age=1000
+)
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/")
+def index(request: Request):
+    return {"message": "Hello World"}
+
+
+@app.get("/v2ray", response_class=HTMLResponse)
+def v2ray(request: Request):
+    context = {"request": request}
+    return templates.TemplateResponse("index.html", context=context)
+
+
+@app.get("/cors")
+def cors(request: Request, url: Optional[str] = None):
+    if url is None:
+        return {"code": 0, "usage": "{HOST}/cors?url={URL}"}
+
+    def stream():
+        response = requests.get(url, stream=True)
+        for chunk in response.iter_content(chunk_size=1024):
+            if chunk:
+                yield chunk
+
+    return StreamingResponse(content=stream())
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("__main__:app", host="127.0.0.1", port=8000, reload=True)
